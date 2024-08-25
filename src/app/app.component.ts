7538729cf1b6fae699b16305219967d3d9f740ba
HanitraMelodie <Hanitra_Melodie@hotmail.fr>
2024-08-25 23:36:01 +0100

    todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  todoArray: string[] = [];
+
+  addTodo(value: string) {
+    if (value.trim()) {
+      this.todoArray.push(value.trim());
+
+    }
+    console.log(this.todoArray);
+  }
+
+  deleteItem(todo: string) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1)
+      }
+    }
+    console.log("delete item")
+  }
+  todoSubmit(value: string) {
+    if (value.trim()) {
+      this.addTodo(value.trim());
+    } else {
+      alert('Field required **');
+    }
+  }
+
 }
+
